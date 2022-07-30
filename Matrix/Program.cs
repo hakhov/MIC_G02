0863ba5540ab1ec38c6482814d95bd28de611dac
hakhov <hakhov@gmail.com>
2022-07-30 18:23:55 +0400

    Matrix, and Dioganals

---
+++ Matrix/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] arr = new int[4, 5];
+//int[,] arr1 =
+//{
+//    {0, 0, 0},
+//    {1, 1, 1},
+//    {2, 2, 2},
+//    {3, 3, 3},
+//};
+
+//for (int i = 0; i < 4; i++)
+//{
+//    for (int j = 0; j < 3; j++)
+//    {
+//        Console.Write($"arr[{i},{j}] = ");
+//        arr[i, j] = int.Parse(Console.ReadLine());
+//    }
+//}
+
+Random r = new();
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = r.Next(-20,30);
+        Console.Write($"{arr[i,j]}\t");
+       
+    }
+    Console.WriteLine();
+}
---
+++ MatrixDioganal/Program.cs
@@ -0,0 +1,48 @@
+﻿Random r = new();
+
+int temp = r.Next(3, 15);
+int n = temp % 2 == 0 ? temp + 1 : temp;
+int[,] arr = new int[n, n];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = r.Next(-20, 30);
+        //Console.Write($"{arr[i, j]}\t");
+
+    }
+    // Console.WriteLine();
+}
+
+Console.WriteLine(new String('_', 50));
+int[] arrI = new int[n];
+int[] arrJ = new int[n];
+
+int index = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = n - 1 - i; j < n - i; j++)
+    {
+        arrI[index] = i;
+        arrJ[index] = j;
+        index++;
+    }
+}
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (i == arrI[i] && j == arrJ[i])
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        else
+        {
+            Console.ResetColor();
+        }
+        Console.Write($"{arr[i, j]}\t");
+    }
+    Console.WriteLine();
+}
